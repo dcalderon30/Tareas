6160922da7385cd4e44471b6460360a72a61079f
dcalderon30 <54612399+dcalderon30@users.noreply.github.com>
2020-02-09 20:15:04 -0600

    Tarea semana 2 added

---
+++ actividad2.js
@@ -0,0 +1,59 @@
+var month = 10;
+
+switch(month) {
+    case 1:
+        console.log("es invierno");
+        console.log("Enero");
+        break;
+    case 2:
+        console.log("es invierno");
+        console.log("Febrero");
+        break;
+    case 3:
+        console.log("es primavera");
+        console.log("Marzo");
+        break;
+    case 4:
+        console.log("es primavera");
+        console.log("Abril");
+        break;
+    case 5:
+        console.log("es primavera");
+        console.log("Mayo");
+        break;
+    case 6:
+        console.log("es verano");
+        console.log("Junio");
+        break;
+    case 7:
+        console.log("es verano");
+        console.log("Julio");
+        break;
+    case 8:
+        console.log("es verano");
+        console.log("Agosto");
+        break;
+    case 9:
+        console.log("es otoño");
+        console.log("Septiembre");
+        break;
+    case 10:
+        console.log("es otoño");
+        console.log("Octubre");
+        break;
+    case 11:
+        console.log("es otoño");
+        console.log("Noviembre");
+        break;
+    case 12:
+        console.log("es invierno");
+        console.log("Diciembre");
+        break;
+    default:
+        console.log("usted ingresó un mes invalido");
+        break;
+}
+
+console.log(month);
+console.log('Douglas Raúl Calderón Bedoya');
+console.log('Carné 20006802');
